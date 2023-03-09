1cc240d10da736543e1aab6224c5835b709c42d2
SimonZeu19 <simon.zeudjiotchiofo@studenti.unipr.it>
2023-03-09 12:23:16 +0100

    Update Registrazione.cs
    Registrazione

--- WebApplication1/Models/Registrazione.cs
+++ WebApplication1/Models/Registrazione.cs
@@ -8,6 +8,36 @@ namespace WebApplication1.Models
 {
     public class Registrazione
     {
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "Nome richiesto")]
+        [MinLength(4, ErrorMessage = "Il nome  non può essere più corto di {1} caratteri")]
+        [MaxLength(254, ErrorMessage = "Il nome non può essere più lungo di {1} caratteri")]
+        public string nome { get; set; }
+
+        [Display(Name = "Cognome")]
+        [Required(ErrorMessage = "Cognome richiesto")]
+        [MinLength(4, ErrorMessage = "Il cognome non può essere più corto di {1} caratteri")]
+        [MaxLength(254, ErrorMessage = "Il cognome non può essere più lungo di {1} caratteri")]
+        public string cognome { get; set; }
+
+        [Display(Name = "Codice fiscale")]
+        [Required(ErrorMessage = "Codice fiscale richiesto")]
+        [MinLength(4, ErrorMessage = "Il codice fiscale non può essere più corto di {1} caratteri")]
+        [MaxLength(254, ErrorMessage = "Il codice fiscale non può essere più lungo di {1} caratteri")]
+        public string Codicefiscale  { get; set; }
+
+        [Display(Name = "Indirizzo di residenza")]
+        [Required(ErrorMessage = "L'Indirizzo di residenza richiesto")]
+        [MinLength(4, ErrorMessage = " Indirizzo di residenza non può essere più corto di {1} caratteri")]
+        [MaxLength(254, ErrorMessage = "L'Indirizzo di residenza non può essere più lungo di {1} caratteri")]
+        public string Indirizzo_residenza { get; set; }
+
+        [Display(Name = "Il numero di cellulare")]
+        [Required(ErrorMessage = "Il numero di cellulare richiesto")]
+        [MinLength(4, ErrorMessage = " Il numero di cellulare non può essere più corto di {1} caratteri")]
+        [MaxLength(254, ErrorMessage = "Il numero di cellulare non può essere più lungo di {1} caratteri")]
+        public string Numero_cellulare { get; set; }
+
         [Display(Name = "Username")]
         [Required(ErrorMessage = "Username richiesto")]
         [MinLength(4, ErrorMessage = "Lo username non può essere più corto di {1} caratteri")]
